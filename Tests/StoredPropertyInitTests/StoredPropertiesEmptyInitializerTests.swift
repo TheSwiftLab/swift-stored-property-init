0fb3ce02d0930bcec97ca583c48b578ce37aecdd
opficdev <162981733+opficdev@users.noreply.github.com>
2026-06-09 14:28:16 +0900

    test: 명시적 storedProperties empty initializer 검증

--- Tests/StoredPropertyInitTests/StoredPropertiesEmptyInitializerTests.swift
+++ Tests/StoredPropertyInitTests/StoredPropertiesEmptyInitializerTests.swift
@@ -26,6 +26,27 @@ final class StoredPropertiesEmptyInitializerTests: XCTestCase {
         )
     }
 
+    /// 명시적 저장 프로퍼티 모드에서도 선택된 파라미터가 없으면 empty initializer를 생성합니다.
+    func testExplicitStoredPropertiesModeGeneratesEmptyInitializerWhenAllPropertiesAreOmitted() throws {
+        assertMacroExpansion(
+            """
+            @StoredPropertyInit(mode: .storedProperties)
+            struct ViewState {
+                var title: String = ""
+            }
+            """,
+            expandedSource: """
+            struct ViewState {
+                var title: String = ""
+
+                init() {
+                }
+            }
+            """,
+            macros: makeTestMacros()
+        )
+    }
+
     /// `defaults: .parameters`에서 initialized `let` 때문에 선택된 파라미터가 없으면 empty initializer를 생성하지 않습니다.
     func testStoredPropertiesModeParametersDoesNotGenerateEmptyInitializerForInitializedLetProperty() throws {
         assertMacroExpansion(
